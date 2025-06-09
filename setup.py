e42c5de37d72b4a84f21606a2c87f0377fd70a87
Mathieu Gravey <mathieu@mgravey.com>
2025-06-09 19:08:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import setup
 
 setup(
 	name='pandoc-smart-cite',
-	version='0.1',
+	version='0.2',
 	description='A Pandoc filter for automatically fetching and caching DOI/arXiv BibTeX entries',
 	author='Mathieu Gravey',
 	author_email='reserach@mgravey.com',
 	url='https://github.com/mgravey/pandoc-smart-cite',
 	py_modules=['smart_cite'],
+	entry_points={
+		'console_scripts': [
+			'smart-cite = smart_cite:main',
+		],
+	},
 	install_requires=[
 		'panflute',
 		'requests',
